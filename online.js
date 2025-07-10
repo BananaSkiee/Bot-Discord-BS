458303fdd4418a60516684d072961e22afdaa0ce
BananaSkiee <bananaskiee@gmail.com>
2025-07-10 15:10:35 +0700

    Update online.js

--- online.js
+++ online.js
@@ -3,13 +3,15 @@ const VOICE_CHANNEL_ID = "1366854862608007329";
 module.exports = async function updateVoice(guild) {
   const ch = guild.channels.cache.get(VOICE_CHANNEL_ID);
   if (!ch?.isVoiceBased()) return;
-  await guild.members.fetch();
+
+  await guild.members.fetch({ withPresences: true });
   const count = ch.members.filter(m =>
     ["online", "idle", "dnd"].includes(m.presence?.status)
   ).size;
+
   try {
     await ch.setName(`「 Online: ${count} 」`);
-    console.log(`✅ Voice channel rename → Online: ${count}`);
+    console.log(`✅ Channel rename → Online: ${count}`);
   } catch (e) {
     console.error("❌ Gagal ganti nama channel:", e);
   }
