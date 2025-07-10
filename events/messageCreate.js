cdbbf74249545b7d800d9e7ff1fa54a9778b2f09
BananaSkiee <bananaskiee@gmail.com>
2025-07-10 22:18:09 +0700

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,10 +1,17 @@
+const scheduler = require("../scheduler");
+
 module.exports = {
   name: "messageCreate",
   async execute(message) {
     if (message.author.bot) return;
 
-    if (message.content.toLowerCase() === "!test") {
-      message.channel.send("✅ Bot aktif dan command `!test` berfungsi!");
+    if (message.content === "!test") {
+      return message.channel.send("✅ Bot aktif dan command `!test` berfungsi!");
+    }
+
+    if (message.content === "!randomchat") {
+      const text = scheduler.getRandomMessage(); // Fungsi dari scheduler
+      return message.channel.send(`🧠 ${text}`);
     }
   }
 };
