c5a27e5360b6748ba9db8eb5ddb2730eabc5b815
BananaSkiee <bananaskiee@gmail.com>
2025-07-10 22:56:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   token: process.env.TOKEN,
-  voiceChannelId: "1366854862608007329",           // voice channel online
-  logChannelId: "1352339757660635197",             // update log channel
-  greetingChannelId: "1352404526870560788"         // chat pagi/siang/malam
+  voiceChannelId: "1366854862608007329",
+  logChannelId: "1352339757660635197",
+  greetingChannelId: "1352404526870560788" // ⬅️ Tambahin ini
 };
