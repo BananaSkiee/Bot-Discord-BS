82b13e22f143050d7276c7e941f11acf65f25ac7
BananaSkiee <bananaskiee@gmail.com>
2025-07-10 18:23:19 +0700

    Delete commands directory

--- commands/online.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "online",
-  execute(message, args) {
-    const count = message.guild.members.cache.filter(
-      m => m.presence && ["online", "idle", "dnd"].includes(m.presence.status) && !m.user.bot
-    ).size;
-    message.reply(`👥 Jumlah member online: ${count}`);
-  }
-};
