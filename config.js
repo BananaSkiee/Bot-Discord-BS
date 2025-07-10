e2a9ee5ec91750f64464f9ff9582aed54d3a6321
BananaSkiee <bananaskiee@gmail.com>
2025-07-10 21:30:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   token: process.env.TOKEN,
-  voiceChannelId: "1366854862608007329" // ID voice channel kamu
+  voiceChannelId: "1366854862608007329", // ID voice channel
+  logChannelId: "135233975766063519"     // Channel log: 「📰」ᴜᴘᴅᴀᴛᴇ-sᴇʀᴠᴇ
 };
