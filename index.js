ecbcf5c72e348c663ec6ada6123f29dcd9310524
BananaSkiee <bananaskiee@gmail.com>
2025-07-10 16:27:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { Client, GatewayIntentBits } = require("discord.js");
-
-const VOICE_CHANNEL_ID = "1366854862608007329";
+const config = require("./config");
+const updateOnline = require("./online");
 
 const bot = new Client({
   intents: [
@@ -11,37 +11,20 @@ const bot = new Client({
   ]
 });
 
+// Keep-alive Railway
 const app = express();
-app.get("/", (_, res) => res.send("Akira aktif 24/7!"));
+app.get("/", (_, res) => res.send("Akira aktif"));
 app.listen(process.env.PORT || 3000, () => console.log("🌐 Server aktif"));
 
-async function updateOnlineCount(guild) {
-  try {
-    await guild.members.fetch({ withPresences: true });
-
-    const onlineCount = guild.members.cache.filter(member =>
-      member.presence && ["online", "idle", "dnd"].includes(member.presence.status) && !member.user.bot
-    ).size;
-
-    const channel = guild.channels.cache.get(VOICE_CHANNEL_ID);
-    if (channel && channel.isVoiceBased()) {
-      await channel.setName(`「 Online: ${onlineCount} 」`);
-      console.log(`✅ Voice channel diupdate: ${onlineCount}`);
-    }
-  } catch (err) {
-    console.error("❌ ERROR update:", err);
-  }
-}
-
 bot.on("ready", () => {
-  console.log(`🤖 Akira login sebagai ${bot.user.tag}`);
+  console.log(`🤖 Login sebagai ${bot.user.tag}`);
   const guild = bot.guilds.cache.first();
-  if (guild) updateOnlineCount(guild);
+  if (guild) updateOnline(guild);
 });
 
-bot.on("presenceUpdate", async (_, newPresence) => {
+bot.on("presenceUpdate", (_, newPresence) => {
   const guild = newPresence.guild;
-  if (guild) updateOnlineCount(guild);
+  if (guild) updateOnline(guild);
 });
 
-bot.login(process.env.TOKEN);
+bot.login(config.token);
