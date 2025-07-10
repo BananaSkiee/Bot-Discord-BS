5d6c9cd40ee53af8707a33a20b7af03a99d5bbf0
BananaSkiee <bananaskiee@gmail.com>
2025-07-10 17:45:52 +0700

    Update online.js

--- online.js
+++ online.js
@@ -5,15 +5,18 @@ module.exports = async function updateOnline(guild) {
     await guild.members.fetch({ withPresences: true });
 
     const count = guild.members.cache.filter(
-      m => m.presence && ["online", "idle", "dnd"].includes(m.presence.status) && !m.user.bot
+      m =>
+        m.presence &&
+        ["online", "idle", "dnd"].includes(m.presence.status) &&
+        !m.user.bot
     ).size;
 
     const channel = guild.channels.cache.get(config.voiceChannelId);
     if (channel && channel.isVoiceBased()) {
       await channel.setName(`「 Online: ${count} 」`);
-      console.log(`✅ Voice channel updated: Online: ${count}`);
+      console.log(`✅ Channel updated: ${count}`);
     }
   } catch (err) {
-    console.error("❌ Failed to update online:", err.message);
+    console.error("❌ Error update:", err);
   }
 };
