552749ab59703da98738266fe784e601ed58eea4
BananaSkiee <bananaskiee@gmail.com>
2025-07-10 17:46:25 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,7 @@
-const updateOnline = require("../online");
-
 module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
     console.log(`🤖 Bot siap sebagai ${client.user.tag}`);
-    const guild = client.guilds.cache.first();
-    if (guild) await updateOnline(guild);
   }
 };
