4cab9f65b3848d092bcc4b0a10d4a533cbc40e07
BananaSkiee <bananaskiee@gmail.com>
2025-07-10 22:50:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   token: process.env.TOKEN,
-  voiceChannelId: "1366854862608007329", // ID voice channel
-  logChannelId: "1352339757660635197",   // Channel log: 「📰」ᴜᴘᴅᴀᴛᴇ-sᴇʀᴠᴇ
-  greetingChannelId: "1352404526870560788" // Channel khusus untuk chat otomatis
+  voiceChannelId: "1366854862608007329",           // voice channel online
+  logChannelId: "1352339757660635197",             // update log channel
+  greetingChannelId: "1352404526870560788"         // chat pagi/siang/malam
 };
