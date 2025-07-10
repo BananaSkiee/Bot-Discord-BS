c57812e178f8f3737eb02b144a3ae71bc07e26fe
BananaSkiee <bananaskiee@gmail.com>
2025-07-10 16:59:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  token: process.env.TOKEN, // Ambil token dari Railway environment
-  voiceChannelId: "1366854862608007329" // Ganti dengan ID voice channel lo
+  token: process.env.TOKEN,
+  voiceChannelId: "1366854862608007329",
+  prefix: "!"
 };
