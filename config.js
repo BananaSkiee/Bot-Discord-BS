22412223c5b97d110a335a286bd9e68f09d2c237
BananaSkiee <bananaskiee@gmail.com>
2025-07-11 11:39:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   token: process.env.TOKEN,
-  voiceChannelId: "1366854862608007329",
-  logChannelId: "1393011303492092035",
-  greetingChannelId: "1352404526870560788" // ⬅️ Tambahin ini
+  voiceChannelId: "1366854862608007329",     // 🔊 Voice Channel "Online"
+  logChannelId: "1393011303492092035",       // 📋 Channel log update online
+  guildId: "1347233781391560837"             // 🏠 ID server kamu
 };
