4364b8bc2647796939812298777b28cc8495772d
BananaSkiee <bananaskiee@gmail.com>
2025-07-11 11:44:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   token: process.env.TOKEN,
-  voiceChannelId: "1366854862608007329",     // 🔊 Voice Channel "Online"
-  logChannelId: "1393011303492092035",       // 📋 Channel log update online
-  guildId: "1347233781391560837"             // 🏠 ID server kamu
+  voiceChannelId: "1366854862608007329",     // 🔊 Voice Channel yang akan diganti namanya jadi "Online: ..."
+  logChannelId: "1393011303492092035",       // 📋 Channel log untuk info update online
+  guildId: "1347233781391560837"             // 🏠 ID server Discord kamu
 };
