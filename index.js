77e519441ffb7518187206cf9c03a542f0b3b3bb
BananaSkiee <bananaskiee@gmail.com>
2025-07-11 11:13:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const config = require("./config");
 
 const stickyHandler = require("./sticky");
 const setupSlashCommands = require("./slashCommandSetup");
-const createVoiceChannel = require("./modules/createVoiceChannel"); // ✅ Tambahan
+const createVoiceChannel = require("./createVoiceChannel"); // ✅ Tambahan
 const handleNickname = require("./modules/nicknameTag"); // ✅ Tambahan
 
 const client = new Client({
