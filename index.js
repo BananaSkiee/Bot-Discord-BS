479398ecd43ec6cdc86881c57a02bf76bdfd1f28
BananaSkiee <bananaskiee@gmail.com>
2025-07-11 12:53:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const config = require("./config");
 
 const stickyHandler = require("./sticky");
-const setupSlashCommands = require("./slashCommandSetup");
+const setupSlashCommands = require("./sticky");
 const updateOnline = require("./online"); // 🔊 Update Online VC
 
 const client = new Client({
