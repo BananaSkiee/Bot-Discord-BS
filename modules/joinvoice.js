7c83cfa7b74954562963d0fdc839c8ac4312db76
BananaSkiee <bananaskiee@gmail.com>
2025-07-19 20:08:10 +0700

    Update joinvoice.js

--- modules/joinvoice.js
+++ modules/joinvoice.js
@@ -2,7 +2,7 @@ const { joinVoiceChannel } = require("@discordjs/voice");
 
 module.exports = async (client) => {
   const guild = await client.guilds.fetch(process.env.GUILD_ID);
-  const channel = guild.channels.cache.get(process.env.VOICE_CHANNEL_ID);
+  const channel = await guild.channels.fetch(process.env.VOICE_CHANNEL_ID); // ✅ fetch, bukan cache
 
   if (!channel || channel.type !== 2) {
     return console.error("❌ Voice channel tidak ditemukan atau bukan voice channel.");
