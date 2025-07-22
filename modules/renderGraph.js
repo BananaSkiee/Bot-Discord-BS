027eeb2cde96537550f47fb49b06736df9804d80
BananaSkiee <bananaskiee@gmail.com>
2025-07-22 18:22:03 +0700

    Update renderGraph.js

--- modules/renderGraph.js
+++ modules/renderGraph.js
@@ -3,18 +3,18 @@ const path = require("path");
 
 async function renderGraph() {
   const browser = await puppeteer.launch({
-    headless: "new", // gunakan 'true' kalau kamu deploy di server
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    headless: "new",
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
+
   const page = await browser.newPage();
+  await page.goto(`file:${path.join(__dirname, "grafik.html")}`);
 
-  await page.goto(`file:${path.join(__dirname, "graph.html")}`);
-  const graphElement = await page.$("#graph-container"); // Pastikan ada ID ini
+  const chartElement = await page.$("#chart");
+  const buffer = await chartElement.screenshot({ omitBackground: true });
 
-  const buffer = await graphElement.screenshot({ omitBackground: true });
   await browser.close();
-
-  return buffer; // Kembalikan sebagai buffer
+  return buffer;
 }
 
 module.exports = renderGraph;
