b8156010cd5ca048738b968df40fec792066ba56
BananaSkiee <bananaskiee@gmail.com>
2025-07-22 15:37:02 +0700

    Create autoMeme.js

---
+++ modules/autoMeme.js
@@ -0,0 +1,20 @@
+// modules/autoMeme.js
+const { EmbedBuilder } = require("discord.js");
+const axios = require("axios");
+
+module.exports = async function autoSendMeme(channel) {
+  try {
+    const res = await axios.get("https://meme-api.com/gimme"); // bebas pakai API lain
+    const meme = res.data;
+
+    const embed = new EmbedBuilder()
+      .setTitle(meme.title)
+      .setImage(meme.url)
+      .setURL(meme.postLink)
+      .setFooter({ text: `Dari: ${meme.subreddit}` });
+
+    await channel.send({ embeds: [embed] });
+  } catch (err) {
+    console.error("❌ Gagal ambil meme:", err);
+  }
+};
