5787838697f89befe676f62e554129beeb079a6f
BananaSkiee <bananaskiee@gmail.com>
2025-07-22 15:36:10 +0700

    Create meme command.js

---
+++ modules/meme command.js
@@ -0,0 +1,26 @@
+// modules/memeCommand.js
+const { EmbedBuilder } = require("discord.js");
+const axios = require("axios");
+
+module.exports = {
+  name: "meme",
+  description: "Mengirim meme acak dari Reddit",
+  async execute(message) {
+    try {
+      const res = await axios.get("https://meme-api.com/gimme");
+      const meme = res.data;
+
+      const embed = new EmbedBuilder()
+        .setTitle(meme.title)
+        .setImage(meme.url)
+        .setURL(meme.postLink)
+        .setFooter({ text: `Dari subreddit: ${meme.subreddit}` })
+        .setColor("Random");
+
+      await message.channel.send({ embeds: [embed] });
+    } catch (err) {
+      console.error("Gagal ambil meme:", err);
+      message.channel.send("❌ Gagal mengambil meme. Coba lagi nanti!");
+    }
+  },
+};
