d58cbed06c9afdbc8b396ab27864596874281f1e
BananaSkiee <bananaskiee@gmail.com>
2025-07-23 20:44:16 +0700

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -11,7 +11,7 @@ const handleHapusTag = require("../modules/hapusTagCommand");
 const translateHandler = require("../modules/translate");
 const memeCommand = require("../modules/memeCommand");
 const textCounter = require("../modules/textCounter");
-const autodelete = require("../modules/autodelete");
+const autodelete = require("../modules/autoDeleteCrypto.js");
 
 const filePath = path.join(__dirname, "../data/taggedUsers.json");
 
