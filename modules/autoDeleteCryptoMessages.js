2b751db640a8f88c53ae00d0fd308cf2220e465c
BananaSkiee <bananaskiee@gmail.com>
2025-07-23 21:14:47 +0700

    Update autoDeleteCryptoMessages.js

--- modules/autoDeleteCryptoMessages.js
+++ modules/autoDeleteCryptoMessages.js
@@ -1,25 +1,14 @@
-const { Events } = require("discord.js");
-const fs = require("fs");
-const path = require("path");
+const CRYPTO_CHANNEL_ID = "1397169936467755151";
 
-const CRYPTO_CHANNEL_ID = "1397169936467755151"; // ID channel cripto
+module.exports = async function autoDeleteCrypto(message) {
+  if (message.author.bot) return;
+  if (message.channel.id !== CRYPTO_CHANNEL_ID) return;
 
-module.exports = {
-  name: Events.MessageCreate,
-  async execute(message) {
-    // Abaikan jika dari bot
-    if (message.author.bot) return;
-
-    // Hanya berlaku di channel crypto
-    if (message.channel.id !== CRYPTO_CHANNEL_ID) return;
-
-    // Simpan pesan untuk dihapus nanti
-    setTimeout(async () => {
-      try {
-        await message.delete();
-      } catch (error) {
-        console.error("Gagal hapus pesan:", error.message);
-      }
-    }, 10 * 1000); // 30 menit
-  },
+  setTimeout(async () => {
+    try {
+      await message.delete();
+    } catch (error) {
+      console.error("Gagal hapus pesan:", error.message);
+    }
+  }, 10_000);
 };
