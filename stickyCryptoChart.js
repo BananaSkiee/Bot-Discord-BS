ec21076299774990864c68e57d1bd0b1f1f9bf7f
BananaSkiee <bananaskiee@gmail.com>
2025-07-23 08:14:18 +0700

    Delete stickyCryptoChart.js

--- stickyCryptoChart.js
+++
@@ -1,8 +0,0 @@
-const hargaList = [900000000, 905000000, 899000000, 910000000, 912000000];
-
-const chartUrl = generateChartUrl("BTC/IDR", hargaList);
-
-await interaction.message.edit({
-  content: "💹 Grafik Harga BTC:",
-  files: [chartUrl],
-});
