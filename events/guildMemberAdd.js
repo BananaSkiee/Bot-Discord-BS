08ac36960417d70e03d69b9fcfee63b4bee80225
BananaSkiee <bananaskiee@gmail.com>
2025-07-27 22:33:08 +0700

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -57,10 +57,15 @@ module.exports = {
 
             // PERUBAHAN: Mengirim pesan lengkap dengan tombol
             await welcomeChannel.send({
-                embeds: [welcomeEmbed],
-                files: [attachment],
-                components: [row] // Menambahkan tombol ke pesan
-            });
+                  content: `<a:BananaSkiee:1360541400382439475> <a:rflx:1361623860205715589> <a:rflx_e:1361624001939771413> <a:rflx_l:1361624056884887673> <a:rflx_c:1361624260434591855> <a:rflx_o:1361624335126626396> <a:rflx_m:1361624355771256956> <a:rflx_e:1361624001939771413> <a:BananaSkiee:1360541400382439475>
+  
+  Welcome       : <@${member.id}>
+To Server     : ${member.guild.name}
+Total Members : ${member.guild.memberCount}`,
+  embeds: [testEmbed], 
+  files: [attachment], 
+  components: [row] 
+});
 
         } catch (error) {
             console.error("ERROR SAAT MEMBUAT WELCOME EMBED DENGAN TOMBOL:", error);
