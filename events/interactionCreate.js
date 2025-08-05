22630af07835bf4aa1bdba520dc213cccd7751dd
MinimalMikir <bananaskiee@gmail.com>
2025-08-06 01:03:12 +0700

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -210,19 +210,19 @@ return interaction.reply({
   content: "⚠️ Tombol tidak dikenali.",
   ephemeral: true,
 });
-
-} catch (err) {
-  console.error("❌ ERROR GLOBAL DI INTERACTIONCREATE:", err);
-
-  if (interaction.replied || interaction.deferred) {
-    await interaction.followUp({
-      content: "❌ Terjadi error internal.",
-      ephemeral: true,
-    });
-  } else {
-    await interaction.reply({
-      content: "❌ Terjadi error internal.",
-      ephemeral: true,
-    });
+    } catch (err) {
+      console.error("❌ ERROR GLOBAL DI INTERACTIONCREATE:", err);
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({
+          content: "❌ Terjadi error internal.",
+          ephemeral: true,
+        });
+      } else {
+        await interaction.reply({
+          content: "❌ Terjadi error internal.",
+          ephemeral: true,
+        });
+      }
+    }
   }
-}
+};
