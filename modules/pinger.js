a82d29ffa27a49a420ba2ea5e93b615a71722d7d
MinimalMikir <bananaskiee@gmail.com>
2025-08-19 18:38:31 +0700

    Update pinger.js

--- modules/pinger.js
+++ modules/pinger.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 // 🔗 daftar URL yg mau di-ping
 const urls = [
-  "https://194a2c34-3a81-4098-8a43-c57019ed739e-00-je64py35b2a3.pike.replit.dev:8080/",
+  "https://194a2c34-3a81-4098-8a43-c57019ed739e-00-je64py35b2a3.pike.replit.dev:8080/"
 ];
 
 async function pingUrl(url) {
