b0ccb39ac89d01bffd6eb58e7d88f1c810cd2de3
MinimalMikir <bananaskiee@gmail.com>
2025-08-19 23:34:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -31,14 +31,13 @@ const invitesTracker = require("./modules/invitesTracker");
 const slashCommandSetup = require("./modules/slashCommandSetup");
 const rulesCommand = require("./modules/rulesCommand");
 const mcModule = require("./modules/minecraft");
-const duelGame = require("./modules/duelGame");
+const gameManager = require("./modules/gameManager");
 require("./modules/srvName")(client); 
 
 client.commands = new Collection();
 
 // 📂 Prefix Commands (Crypto Game)
 const prefixCommands = {
-  duel: duelGame.startDuel,
   register: cmdCrypto.register,
   balance: cmdCrypto.balance,
   help: cmdCrypto.help,
